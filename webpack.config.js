717faed92c8139d2f5e82fd96b16a9dcd00f15b4
chuck <596659597@qq.com>
2026-06-18 16:40:56 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'resource', to: 'resource' },
+      ],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname),
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+};
